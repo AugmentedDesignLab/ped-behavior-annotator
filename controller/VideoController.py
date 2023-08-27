2514941a4d4896b8ebb363847ac83defeca52844
srishtisripada <srishti.sripada@gmail.com>
2023-08-27 12:46:15 -0700

    Update VideoController.py

--- controller/VideoController.py
+++ controller/VideoController.py
@@ -1,22 +1,33 @@
 import cv2
 
 class VideoController:
-
     def __init__(self, capture: cv2.VideoCapture) -> None:
         self.capture = capture
-        pass
 
     def getNFrames(self) -> int:
-        pass
+        #Total number of frames in the video
+        return int(self.capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
     def getFPS(self) -> float:
-        pass
+        #FPS of the video
+        return self.capture.get(cv2.CAP_PROP_FPS)
 
     def getDuration(self) -> float:
-        pass
+        #Duration of the video in seconds
+        total_frames = self.getNFrames()
+        fps = self.getFPS()
+        if fps == 0:
+            return 0
+        return total_frames / fps
 
     def getFrameAfter(self, currentFrame: int, duration: float) -> int:
-        pass
+        #The frame number after the current frame or duration
+        fps = self.getFPS()
+        frame_diff = int(duration * fps)
+        return currentFrame + frame_diff
 
     def getFrameBefore(self, currentFrame: int, duration: float) -> int:
-        pass
+        #Frame number before the current frame or duration
+        fps = self.getFPS()
+        frame_diff = int(duration * fps)
+        return max(currentFrame - frame_diff, 0)
