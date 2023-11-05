5e975878b9e869d101ae1ba1214977866a35c135
rsaravanan23 <rishi.saravanan123@gmail.com>
2023-11-04 17:02:19 -0700

    RecordingController and unit tests
    Wrote unit tests for the addSingleFrameAnnotation and addMultiFrameAnnotation method in the recordingController class. There is currently an import error with 'model' which I am trying to fix.

---
+++ tests/controller/test_RecordingController.py
@@ -0,0 +1,39 @@
+from asyncio.windows_events import NULL
+from tokenize import Single
+from model.MultiFrameAnnotation import MultiFrameAnnotation
+from model.SingleFrameAnnotation import SingleFrameAnnotation
+from model.Recording import Recording
+from model.RecordingRepository import RecordingRepository
+from controller.RecordingController import RecordingController
+import pytest
+
+
+@pytest.fixture
+def singleAnnotation() -> SingleFrameAnnotation:
+    singleFrame = SingleFrameAnnotation()
+    return singleFrame
+
+@pytest.fixture
+def multiAnnotation() -> MultiFrameAnnotation:
+    multiFrame = MultiFrameAnnotation()
+    return multiFrame
+
+def test_addSingleFrameAnnotation(singleAnnotation):
+    # Create a Repository object and Recording Object. 
+    recording = Recording("")
+    video_url = "https://www.youtube.com/watch?v=eu4QqwsfXFE"
+    repository = RecordingRepository(video_url)
+    recordingController = RecordingController(repository, recording)
+    recordingController.addSingleFrameAnnotation(singleAnnotation)
+
+    # Assert for not null value. 
+    assert recordingController.getRecordingByVideoPath(video_url) != NULL
+
+def test_addMultiFrameAnnotation():
+    recording = Recording("")
+    video_url = "https://www.youtube.com/watch?v=eu4QqwsfXFE"
+    repository = RecordingRepository(video_url)
+    recordingController = RecordingController(repository, recording)
+    recordingController.addMultiFrameAnnotation(multiAnnotation)
+
+    assert recordingController.getRecordingByVideoPath(video_url) != NULL
