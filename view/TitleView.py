cc7084f15dd3cb2d68b64d857005533e0b8dbfa9
AmeliaY07 <ameliayuan07@gmail.com>
2024-01-13 19:30:36 -0800

    updated title view with youtube link

--- view/TitleView.py
+++ view/TitleView.py
@@ -1,9 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
+import TKinterModernThemes as TKMT
 
 class TitleView(tk.Frame):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
 
-        newProjLabel = ttk.Label(master=self, text="Add new project", font=20)
-        newProjLabel.grid(row=0, column=0, padx=10, pady=10)
+        newProjButton = ttk.Button(master=self, text="Add new project", font=20, command=self._renderTitleView)
+        newProjButton.grid(row=0, column=0, padx=10, pady=10)
+        newProjButton.invoke()
+
+    def _renderTitleView(self, parent: TKMT.WidgetFrame):
+        self.input_frame = parent.addLabelFrame("Title View", rowspan=2)
+        self.textinputvar.trace_add('Add YouTube link here', parent.textupdate)
+        self.input_frame.Entry(parent.textinputvar, validatecommand=parent.validateText)
